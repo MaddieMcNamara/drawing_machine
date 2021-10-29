25e73cb3c8dcb93099f8f5cb0b844b4297022932
MaddieMcNamara <91571511+MaddieMcNamara@users.noreply.github.com>
2021-10-28 19:34:03 -0700

    drawing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,66 @@
+let array = [];
+let backgroundColor = 200;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(backgroundColor);
 }
 
 function draw() {
-  background(220);
+  background(200);
+  strokeWeight(5);
+  noFill();
+
+if (mouseIsPressed){
+  backgroundColor -= 5;
+  background(backgroundColor);
+
+// stroke(map(mouseX, 0, 600, 0, 255, true), map(mouseX, 0, 600, 0, 255, true))
+//
+// line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+
+line(mouseX, mouseY, pmouseX, pmouseY);
+array.push([mouseX, mouseY]);
+
+beginShape();
+for(let i = 0; i < array.length - 1; i++){
+
+curveVertex(array[i][0], array[i][1])
+}
+endShape();
+
+}
+}
+
+
+function keyTyped() {
+  if (key === 's') {
+    //save image
+    saveCanvas('fileName', 'jpg');
+
+} else if (key === 'd') {
+  background(255);
+
+  beginShape();
+  for(let i = 0; i < array.length; i++){
+
+curveVertex(array[i][0], array[i][1])
+}
+endShape();
+  //display image
+
+
+
+}
+
+
+return false;
+}
+
+function mousePressed () {
+
+array = [];
+backgroundColor = 255;
+
 }
